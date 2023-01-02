6445274891236e748983aa98e0f0c9aa113e152f
Memresable <kravquexa@gmail.com>
2023-01-02 21:36:47 +0300

    funny update

--- source/core/utilities/general_utilities.h
+++ source/core/utilities/general_utilities.h
@@ -18,44 +18,16 @@ OutputDebugStringA(err_string); \
 exit(EXIT_FAILURE); \
 }
 
-typedef struct
-{
-    char* data;
-    uint32_t size;
-} StringData;
 inline int
 compareTwoStrings(string_t f_string1, string_t f_string2)
 {
-    StringData stringsData[2] = {0};
-    
-    for(uint32_t j = 0; j < 2; j++)
+    for(uint32_t i = 0; f_string1[i] != '\0' && f_string2[i] != '\0'; i++)
     {
-        for(uint32_t i = 0; ((j == 1) ? f_string1[i] : f_string2[i]) != '\0'; i++)
-        {
-            stringsData[j].size += 1;
-        }
-    }
-    if(stringsData[0].size != stringsData[1].size) return(MEMRE_FALSE);
-    
-    stringsData[0].data = (char*)calloc(1, sizeof(char)*stringsData[0].size);
-    stringsData[1].data = (char*)calloc(1, sizeof(char)*stringsData[1].size);
-    
-    for(uint32_t j = 0; j < 2; j++)
-    {
-        for(uint32_t i = 0; i < stringsData[j].size; i++)
-        {
-            stringsData[j].data[i] = (j == 1) ? f_string1[i] : f_string2[i];
-        }
-    }
-    
-    for(uint32_t i = 0; stringsData[0].data[i] != '\0' && stringsData[1].data[i] != '\0'; i++)
-    {
-        if(stringsData[0].data[i] != stringsData[1].data[i])
+        if(f_string1[i] != f_string2[i])
         {
             return(MEMRE_FALSE);
         }
     }
-    
     return(MEMRE_TRUE);
 }
 
